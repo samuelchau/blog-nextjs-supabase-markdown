307eacc3bc016e2dad09f51ba521437918b8109e
Samuel Chau <samuel.chau@mail.mcgill.ca>
2022-05-23 17:55:38 +0000

    Implemented basic navigation and layout styling of the app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,47 @@
+export default MyApp
+
+import Link from 'next/link'
+import { useState, useEffect } from 'react'
+import { supabase } from '../api'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    const { data: authListener } = supabase.auth.onAuthStateChange(
+      async () => checkUser()
+    )
+    checkUser()
+    return () => {
+      authListener?.unsubscribe()
+    };
+  }, [])
+  async function checkUser() {
+    const user = supabase.auth.user()
+    setUser(user)
+  }
+  return (
+  <div>
+    <nav className="p-6 border-b border-gray-300">
+      <Link href="/">
+        <span className="mr-6 cursor-pointer">Home</span>
+      </Link>
+      {
+        user && (
+          <Link href="/create-post">
+            <span className="mr-6 cursor-pointer">Create Post</span>
+          </Link>
+        )
+      }
+      <Link href="/profile">
+        <span className="mr-6 cursor-pointer">Profile</span>
+      </Link>
+    </nav>
+    <div className="py-8 px-16">
+      <Component {...pageProps} />
+    </div>
+  </div>
+  )
 }
 
 export default MyApp
